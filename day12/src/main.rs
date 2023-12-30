cfca463cdb3db8e506b568025933f0f30cc2be5b
unintendedfraud <briceunintendedfraud@fastmail.com>
2023-12-30 17:38:30 +0100

    z

---
+++ day12/src/main.rs
@@ -0,0 +1,18 @@
+struct Line {
+    conditions: Vec<usize>,
+    record: Vec<char>,
+}
+
+fn main() {
+    let data = get_formatted_data(&util::get_data("data.txt"));
+}
+
+fn get_formatted_data(data: &Vec<String>) -> Vec<Line> {
+    let v = Vec::new();
+
+    for i in 0..data.len() {
+        let line = data[i];
+    }
+
+    return v;
+}
